2749ef2b50794743bd736eebafc78d74d65687cf
DjordjeCvijic <djordjecvijic96@gmail.com>
2021-10-13 12:06:17 +0200

    initial commit

---
+++ Conference organizer-backend/src/main/java/com/example/conferenceorganizerbackend/ConferenceOrganizerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.conferenceorganizerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConferenceOrganizerBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConferenceOrganizerBackendApplication.class, args);
+    }
+
+}
---
+++ Conference organizer-backend/src/test/java/com/example/conferenceorganizerbackend/ConferenceOrganizerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.conferenceorganizerbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConferenceOrganizerBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
